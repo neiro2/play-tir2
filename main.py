ad3381b421f2015def8ccb005d59f7db0e194b0a
neiro2 <neirouniversiti.@gmail.com>
2024-12-19 17:40:26 +0300

    add all  code const

--- main.py
+++ main.py
@@ -3,6 +3,35 @@ import random
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Игра Тир')
+
+icon = pygame.image.load('img/icon.png')
+pygame.display.set_icon(icon)
+
+target_image = pygame.image.load('img/target.png')
+target_width = 80
+target_height = 80
+
+# Создаем список мишеней
+targets = []
+num_targets = 5
+
+for _ in range(num_targets):
+    target = {
+        'x': random.randint(0, SCREEN_WIDTH - target_width),
+        'y': random.randint(0, SCREEN_HEIGHT - target_height),
+        'color': (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)),
+        'points': random.randint(1, 10)
+    }
+    targets.append(target)
+
+misses = 0
+score = 0
+
 
 
 
